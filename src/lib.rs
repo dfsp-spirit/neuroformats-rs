3d4b5f30308e34c58447bf429f51d1616c12b21f
Tim Schäfer <ts+code@rcmd.org>
2021-02-19 20:31:44 +0100

    include fs_surf file from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! Rust implementation of several structural neuroimaging file formats.
+//!
+//! The focus of this package is on surface-based MRI data as produced by FreeSurfer.
+
+pub mod fs_curv;
+
+pub use fs_curv::CurvHeader;
