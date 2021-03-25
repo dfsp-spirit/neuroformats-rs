97e7f2bc44fcdeeb3958e9ff1add1627bdef008c
Tim Schäfer <ts+code@rcmd.org>
2021-03-25 14:17:18 +0100

    make coord funcs pub

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,7 @@ pub mod fs_mgh;
 
 
 pub use fs_curv::{FsCurvHeader, FsCurv, read_curv, write_curv};
-pub use fs_surface::{FsSurfaceHeader, FsSurface, BrainMesh, read_surf};
+pub use fs_surface::{FsSurfaceHeader, FsSurface, BrainMesh, read_surf, coord_center, coord_extrema};
 pub use fs_label::{FsLabel, read_label};
 pub use fs_annot::{FsAnnot, FsAnnotColortable, read_annot};
 pub use fs_mgh::{FsMgh, FsMghHeader, FsMghData, read_mgh, MRI_UCHAR, MRI_INT, MRI_FLOAT, MRI_SHORT};
-
-
