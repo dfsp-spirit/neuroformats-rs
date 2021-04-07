c24381e72275ae5ef0fb32b7bbe0d3179a176062
Tim Schäfer <ts+code@rcmd.org>
2021-04-07 17:05:39 +0200

    mess with Cstring writing, WIP broken atm

--- src/fs_surface.rs
+++ src/fs_surface.rs
@@ -172,7 +172,12 @@ pub fn write_surf<P: AsRef<Path> + Copy>(path: P, surf : &FsSurface) -> std::io:
     f.write_u8(surf.header.surf_magic[1])?;
     f.write_u8(surf.header.surf_magic[2])?;
 
-    f.write_all(b"Some surface\0")?;
+    //f.write_all(b"Some surface\0")?;
+    f.write_all(b"Some surface\x00")?;
+    //f.write_all(&[b'f', b'o', b'o', b'\0'])?;
+
+    let output = std::ffi::CString::new("test").unwrap();
+    f.write(output.as_bytes_with_nul())?;
 
     f.write_i32(surf.header.num_vertices)?;
     f.write_i32(surf.header.num_faces)?;
