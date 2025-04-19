b98adab37d021a373ab0faa19673f5b53396c416
Tim Schäfer <tim.schaefer@ae.mpg.de>
2025-04-19 15:36:33 +0200

    NEW: add example that visualizes brain mesh live in bevy engine

---
+++ examples/brain_bevy/src/main.rs
@@ -0,0 +1,69 @@
+// This example loads a brain mesh and displays it in a 3D scene using the Bevy game engine.
+//
+// Note that bevy requires some system dependencies to be installed. Under Ubuntu, you can install them with:
+//
+// ```bash
+// sudo apt install pkg-config librust-alsa-sys-dev librust-libudev-sys-dev
+// ```
+
+use bevy::{
+    pbr::{CascadeShadowConfigBuilder, DirectionalLightShadowMap},
+    prelude::*,
+};
+use std::f32::consts::*;
+
+fn main() {
+    App::new()
+        .insert_resource(DirectionalLightShadowMap { size: 4096 })
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, animate_light_direction)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(100., 100., 100.).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
+        //EnvironmentMapLight {
+        //diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
+        //specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
+        //intensity: 250.0,
+        //..default()
+        //},
+    ));
+
+    commands.spawn((
+        DirectionalLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        // This is a relatively small scene, so use tighter shadow
+        // cascade bounds than the default for better quality.
+        // We also adjusted the shadow map to be larger since we're
+        // only using a single cascade.
+        CascadeShadowConfigBuilder {
+            num_cascades: 1,
+            maximum_distance: 1.6,
+            ..default()
+        }
+        .build(),
+    ));
+    commands.spawn(SceneRoot(asset_server.load(
+        GltfAssetLabel::Scene(0).from_asset("lh_mesh_sulc_viridis.gltf"),
+    )));
+}
+
+fn animate_light_direction(
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<DirectionalLight>>,
+) {
+    for mut transform in &mut query {
+        transform.rotation = Quat::from_euler(
+            EulerRot::ZYX,
+            0.0,
+            time.elapsed_secs() * PI / 5.0,
+            -FRAC_PI_4,
+        );
+    }
+}
