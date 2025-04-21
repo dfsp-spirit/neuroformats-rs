f548c428d6995d737e06ec34c5e7ff475cd5b82f
Tim Schäfer <ts+code@rcmd.org>
2025-04-21 12:09:16 +0200

    NEW: finish brain_export example app

--- examples/brain_export/src/main.rs
+++ examples/brain_export/src/main.rs
@@ -1,55 +1,28 @@
 ///
-/// brain_export -- neuroformats.rs example application that exports a brain mesh with vertex colors
+/// brain_export -- neuroformats.rs example application that exports a brain mesh with vertex colors based on the sulcal depth.
 ///
 /// This file is part of neuroformats.rs, see https://github.com/dfsp-spirit/neuroformats-rs
 ///
+/// To run this application, run 'cargo run --release' in the examples/brain_export directory.
 
 fn main() {
+    println!("=====[ brain_export -- neuroformats.rs mesh export example application ]=====");
     let lh_surf_file = "../../resources/subjects_dir/subject1/surf/lh.white";
     let rh_surf_file = "../../resources/subjects_dir/subject1/surf/rh.white";
+    let lh_sulc_file = "../../resources/subjects_dir/subject1/surf/lh.sulc";
+    let rh_sulc_file = "../../resources/subjects_dir/subject1/surf/rh.sulc";
 
+    println!("Reading meshes...");
     let lh_surf = neuroformats::read_surf(lh_surf_file).unwrap();
     let rh_surf = neuroformats::read_surf(rh_surf_file).unwrap();
 
-    let lh_sulc =
-        neuroformats::read_curv("../../resources/subjects_dir/subject1/surf/lh.sulc").unwrap();
-    let rh_sulc =
-        neuroformats::read_curv("../../resources/subjects_dir/subject1/surf/rh.sulc").unwrap();
-
-    // Read desikan atlas for the subject
-    let lh_annot =
-        neuroformats::read_annot("../../resources/subjects_dir/subject1/label/lh.aparc.annot")
-            .unwrap();
-    let regions: Vec<String> = lh_annot.regions();
-
-    // Print a list of the regions in the left hemisphere
-    println!("Regions in the left hemisphere:");
-    for region in &regions {
-        println!(" * brain region: {}", region);
-    }
-
-    // Compute the mean sulcal depth for the bankssts region
-    let region_verts_bankssts: Vec<usize> = lh_annot.region_vertices(String::from("bankssts"));
-    // regions_verts_bankssts contains the indices of the vertices in the banksts region. Their index is identical to the index in the lh_sulc array.
-    let bankssts_sulc: Vec<f32> = region_verts_bankssts
-        .iter()
-        .map(|&i| lh_sulc.data[i])
-        .collect();
-    let bankssts_mean_sulc: f32 = bankssts_sulc.iter().sum::<f32>() / bankssts_sulc.len() as f32;
     println!(
-        "Mean sulcal depth for bankssts region: {}",
-        bankssts_mean_sulc
+        "Computing vertex colors using viridis colormap based on sulcal depth per-vertex values..."
     );
+    let lh_colors = lh_surf.colors_from_curv_file(lh_sulc_file).unwrap();
+    let rh_colors = rh_surf.colors_from_curv_file(rh_sulc_file).unwrap();
 
-    //let lh_colors = lh_surf
-    //    .colors_from_curv_file("../../resources/subjects_dir/subject1/surf/lh.sulc")
-    //    .unwrap();
-    let rh_colors = rh_surf
-        .colors_from_curv_file("../../resources/subjects_dir/subject1/surf/rh.sulc")
-        .unwrap();
-
-    let lh_colors = lh_annot.vertex_colors(false, 0);
-
+    println!("Constructing and centering merged mesh and colors from both hemisperes......");
     let mut brain = lh_surf.mesh.merge(&rh_surf.mesh);
     brain.move_to(brain.center().unwrap()); // center the mesh at origin
 
@@ -63,7 +36,7 @@ fn main() {
     // get path of current directory as &path::Path
     let current_dir = std::env::current_dir().unwrap();
 
-    const EXPORT_FILE: &str = "brainmesh.ply";
+    const EXPORT_FILE: &str = "brainmesh_sulc.ply";
     let export_path = current_dir.join(EXPORT_FILE);
     let export_path = export_path.to_str().unwrap();
 
